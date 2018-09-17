fa6f261df614d3be2394ecb31ec02fc968653503
kysry <kysryinst@gmail.com>
2018-09-18 07:22:56 +0900

    login_logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  root 'users#new'
-  get  '/signup',  to: 'users#new'
-  post '/signup',  to: 'users#create'
+  root   'sessions#new'
+  get    '/signup',  to: 'users#new'
+  post   '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
