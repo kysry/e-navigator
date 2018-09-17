81794a603f4429baceba5059755f2f0a4a5f922e
kysry <kysryinst@gmail.com>
2018-09-18 06:20:09 +0900

    user registration heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
+  root 'users#new'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
   resources :users
-
-  get 'home/top'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
