95a80d756b97c4fb37408fd25e6672e720793580
kysry <kysryinst@gmail.com>
2018-09-18 05:45:19 +0900

    user registration check

---
+++ db/migrate/20180917190030_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :password_digest
+      t.string :remember_token
+      t.string :name
+      t.date :birthday
+      t.integer :gender
+      t.string :school
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180917190030) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.string "remember_token"
+    t.string "name"
+    t.date "birthday"
+    t.integer "gender"
+    t.string "school"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
