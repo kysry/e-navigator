6f4ada62344e32fd37820b5ebd233b941c53aa97
kysry <kysryinst@gmail.com>
2018-11-01 03:34:28 +0900

    日本語化にあわせて修正

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,6 @@ module ENavigator
     config.generators.system_tests = nil
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
   end
 end
